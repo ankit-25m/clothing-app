15de49fc81799ac4f8793fe76eee4e8f99b3bdcd
ankit-25m <mangalankit843@gmail.com>
2021-08-17 14:52:02 +0530

    updated our app with header component and logo

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import './App.css'
 import { Route } from 'react-router'
 import HomePage from './Pages/homepage/homepage.component'
 import ShopPage from './Pages/shop/shop.component'
-
+import Header from './components/header/header.component'
 function App() {
   return (
     <div>
+      <Header />
       <Route exact path='/' component={HomePage} />
       <Route path='/shop' component={ShopPage} />
     </div>
